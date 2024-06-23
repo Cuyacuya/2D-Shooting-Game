558926143e8d5105ca01d85e87de9aec3297ca24
Cuyacuya <59507274+Cuyacuya@users.noreply.github.com>
2024-06-24 00:59:55 +0900

    #2 총알 발사 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
     public bool isTouchTop;
     public bool isTouchBottom;
     public bool isTouchRight;
     public bool isTouchLeft;
+
+    public GameObject bulletObjA; //총알 프리팹을 저장할 변수
+    public GameObject bulletObjB; //총알 프리팹을 저장할 변수
+
+    public float speed;
+    public float power;
+    public float maxShotDelay; //장전하는데 거리는 시간
+    public float curShotDelay; //실제 딜레이
 
     Animator anim;
 
@@ -17,6 +24,13 @@ public class Player : MonoBehaviour
         anim = GetComponent<Animator>(); //변수 초기화하는 방법
     }
     void Update()
+    {
+        Move();
+        Fire();
+        Reload();
+    }
+    
+    void Move()
     {
         float h = Input.GetAxisRaw("Horizontal"); //GetAxisRaw는 -1, 0, 1의 값만 들어옴
         if ((isTouchRight && h == 1) || (isTouchLeft && h == -1)) h = 0;
@@ -28,10 +42,57 @@ public class Player : MonoBehaviour
 
         transform.position = curPos + nextPos;
 
-        if((Input.GetButtonDown("Horizontal")) || (Input.GetButtonUp("Horizontal")))
+        if ((Input.GetButtonDown("Horizontal")) || (Input.GetButtonUp("Horizontal")))
         {
             anim.SetInteger("Input", (int)h);
         }
+    }
+
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1")) //Fire1 이라는 버튼을 누르지않는다면(!) 아무것도 안함
+            return;
+
+        if (curShotDelay < maxShotDelay) //현재의 딜레이가 장전 시간보다 작으면 아무것도 안함
+            return;
+
+        switch (power)
+        {
+            case 1:
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.33f, transform.rotation);
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletC = Instantiate(bulletObjB, transform.position, transform.rotation); //큰 총알(bulletB)사용
+                Rigidbody2D rigidC = bulletC.GetComponent<Rigidbody2D>();
+                rigidC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position + Vector3.left * 0.33f, transform.rotation);
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+
+        curShotDelay = 0; //총알을 쏜 뒤 딜레이 변수 0으로 초기화
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
